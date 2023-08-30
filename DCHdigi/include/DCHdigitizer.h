32e4a1ea44aaf33e38111520bac78d8f6c70fb0f
Brieuc Francois <brieuc.francois@cern.ch>
2023-08-30 17:10:06 +0200

    First version of a drift chamber digitizer (#6)
    * [DCHDigitizer] WIP for the smearing of the simhit position
    
    * First version of a simple drift chamber digitizer
    
    * Add some comments
    
    * Fix a cmake warning

--- DCHdigi/include/DCHdigitizer.h
+++ DCHdigi/include/DCHdigitizer.h
@@ -3,17 +3,25 @@
 // GAUDI
 #include "Gaudi/Property.h"
 #include "GaudiAlg/GaudiAlgorithm.h"
+#include "GaudiKernel/IRndmGenSvc.h"
+#include "GaudiKernel/RndmGenerators.h"
 
 // K4FWCORE
 #include "k4FWCore/DataHandle.h"
+#include "k4Interface/IGeoSvc.h"
 
 // EDM4HEP
 #include "edm4hep/SimTrackerHitCollection.h"
 #include "edm4hep/TrackerHitCollection.h"
 
+// DD4HEP
+#include "DDSegmentation/BitFieldCoder.h"
+#include "DD4hep/Detector.h" // for dd4hep::VolumeManager
+
 /** @class DCHdigitizer
  *
- *  Algorithm for creating digitized (meaning 'reconstructed' for now) drift chamber hits (edm4hep::TrackerHit) from Geant4 hits (edm4hep::SimTrackerHit).
+ *  Algorithm for creating digitized drift chamber hits (still based on edm4hep::TrackerHit) from edm4hep::SimTrackerHit.
+ *  You have to specify the expected resolution in z and in xy (distance to the wire). The smearing is applied in the wire reference frame.
  *  
  *  @author Brieuc Francois
  *  @date   2023-03
@@ -42,4 +50,26 @@ private:
   DataHandle<edm4hep::SimTrackerHitCollection> m_input_sim_hits{"inputSimHits", Gaudi::DataHandle::Reader, this};
   // Output digitized tracker hit collection name
   DataHandle<edm4hep::TrackerHitCollection> m_output_digi_hits{"outputDigiHits", Gaudi::DataHandle::Writer, this};
+
+  // Detector readout name
+  Gaudi::Property<std::string> m_readoutName{this, "readoutName", "CDCHHits", "Name of the detector readout"};
+  // Pointer to the geometry service
+  ServiceHandle<IGeoSvc> m_geoSvc;
+  // Decoder for the cellID
+  dd4hep::DDSegmentation::BitFieldCoder* m_decoder;
+  // Volume manager to get the physical cell sensitive volume
+  dd4hep::VolumeManager m_volman;
+
+  // z position resolution in mm
+  FloatProperty m_z_resolution{this, "zResolution", 1.0, "Spatial resolution in the z direction (from reading out the wires at both sides) [mm]"};
+  // xy resolution in mm
+  FloatProperty m_xy_resolution{this, "xyResolution", 0.1, "Spatial resolution in the xy direction [mm]"};
+
+  // Random Number Service
+  IRndmGenSvc* m_randSvc;
+  // Gaussian random number generator used for the smearing of the z position
+  Rndm::Numbers m_gauss_z;
+  // Gaussian random number generator used for the smearing of the xy position
+  Rndm::Numbers m_gauss_xy;
+
 };
--- DCHdigi/src/DCHdigitizer.cpp
+++ DCHdigi/src/DCHdigitizer.cpp
@@ -1,28 +1,97 @@
 #include "DCHdigitizer.h"
 
+// DD4hep
+#include "DDRec/Vector3D.h"
+#include "DD4hep/Detector.h"
+
+// ROOT
+#include "Math/Cylindrical3D.h"
+
 DECLARE_COMPONENT(DCHdigitizer)
 
-DCHdigitizer::DCHdigitizer(const std::string& aName, ISvcLocator* aSvcLoc) : GaudiAlgorithm(aName, aSvcLoc) {
+DCHdigitizer::DCHdigitizer(const std::string& aName, ISvcLocator* aSvcLoc) : GaudiAlgorithm(aName, aSvcLoc), m_geoSvc("GeoSvc", "DCHdigitizer") {
   declareProperty("inputSimHits", m_input_sim_hits, "Input sim tracker hit collection name");
   declareProperty("outputDigiHits", m_output_digi_hits, "Output digitized tracker hit collection name");
 }
 
 DCHdigitizer::~DCHdigitizer() {}
 
-StatusCode DCHdigitizer::initialize() { return StatusCode::SUCCESS; }
+StatusCode DCHdigitizer::initialize() {
+
+  // Initialize random services
+  if (service("RndmGenSvc", m_randSvc).isFailure()) {
+    error() << "Couldn't get RndmGenSvc!" << endmsg;
+    return StatusCode::FAILURE;
+  }
+  if (m_gauss_z.initialize(m_randSvc, Rndm::Gauss(0., m_z_resolution)).isFailure()) {
+    error() << "Couldn't initialize RndmGenSvc!" << endmsg;
+    return StatusCode::FAILURE;
+  }
+  if (m_gauss_xy.initialize(m_randSvc, Rndm::Gauss(0., m_xy_resolution)).isFailure()) {
+    error() << "Couldn't initialize RndmGenSvc!" << endmsg;
+    return StatusCode::FAILURE;
+  }
+
+  // check if readout exists
+  if (m_geoSvc->lcdd()->readouts().find(m_readoutName) == m_geoSvc->lcdd()->readouts().end()) {
+    error() << "Readout <<" << m_readoutName << ">> does not exist." << endmsg;
+    return StatusCode::FAILURE;
+  }
+  // set the cellID decoder
+  m_decoder = m_geoSvc->lcdd()->readout(m_readoutName).idSpec().decoder();
+  // retrieve the volume manager
+  m_volman = m_geoSvc->lcdd()->volumeManager();
+
+  return StatusCode::SUCCESS;
+}
 
 StatusCode DCHdigitizer::execute() {
   // Get the input collection with Geant4 hits
   const edm4hep::SimTrackerHitCollection* input_sim_hits = m_input_sim_hits.get();
-  verbose() << "Input Sim Hit collection size: " << input_sim_hits->size() << endmsg;
+  debug() << "Input Sim Hit collection size: " << input_sim_hits->size() << endmsg;
 
   // Digitize the sim hits
   edm4hep::TrackerHitCollection* output_digi_hits = m_output_digi_hits.createAndPut();
   for (const auto& input_sim_hit : *input_sim_hits) {
     auto output_digi_hit = output_digi_hits->create();
-    output_digi_hit.setEDep(input_sim_hit.getEDep());
-    output_digi_hit.setPosition(input_sim_hit.getPosition());
+    // smear the hit position: need to go in the wire local frame to smear in the direction aligned/perpendicular with the wire for z/distanceToWire, taking e.g. stereo angle into account 
+    // retrieve the cell detElement
+    dd4hep::DDSegmentation::CellID cellID = input_sim_hit.getCellID();
+    auto cellDetElement = m_volman.lookupDetElement(cellID);
+    // retrieve the wire (in DD4hep 1.23 there is no easy way to access the volume daughters we have to pass by detElements, in later versions volumes can be used)
+    const std::string& wireDetElementName = Form("superLayer_%d_layer_%d_phi_%d_wire", m_decoder->get(cellID, "superLayer"), m_decoder->get(cellID, "layer"), m_decoder->get(cellID, "phi"));
+    dd4hep::DetElement wireDetElement = cellDetElement.child(wireDetElementName);
+    // get the transformation matrix used to place the wire
+    const auto& wireTransformMatrix = wireDetElement.nominal().worldTransformation();
+    // Retrieve global position in mm and apply unit transformation (translation matrix is tored in cm)
+    double simHitGlobalPosition[3] = {input_sim_hit.getPosition().x * dd4hep::mm, input_sim_hit.getPosition().y * dd4hep::mm, input_sim_hit.getPosition().z * dd4hep::mm};
+    double simHitLocalPosition[3] = {0, 0, 0};
+    // get the simHit coordinate in cm in the wire reference frame to be able to apply smearing of radius perpendicular to the wire
+    wireTransformMatrix.MasterToLocal(simHitGlobalPosition, simHitLocalPosition);
+    debug() << "Cell ID string: " << m_decoder->valueString(cellID) << endmsg;;
+    debug() << "Global simHit x " << simHitGlobalPosition[0] << " --> Local simHit x " << simHitLocalPosition[0] << " in cm" << endmsg;
+    debug() << "Global simHit y " << simHitGlobalPosition[1] << " --> Local simHit y " << simHitLocalPosition[1] << " in cm" << endmsg;
+    debug() << "Global simHit z " << simHitGlobalPosition[2] << " --> Local simHit z " << simHitLocalPosition[2] << " in cm" << endmsg;
+    // build a vector to easily apply smearing of distance to the wire
+    dd4hep::rec::Vector3D simHitLocalPositionVector(simHitLocalPosition[0], simHitLocalPosition[1], simHitLocalPosition[2]);
+    // get the smeared distance to the wire (cylindrical coordinate as the smearing should be perpendicular to the wire)
+    double smearedDistanceToWire = simHitLocalPositionVector.rho() + m_gauss_xy.shoot() * dd4hep::mm;
+    // smear the z position (in local coordinate the z axis is aligned with the wire i.e. it take the stereo angle into account);
+    double smearedZ = simHitLocalPositionVector.z() + m_gauss_z.shoot() * dd4hep::mm;
+    // build the local position vector of the smeared hit using cylindrical coordinates. When we will have edm4hep::DCHit there will be probably no need
+    ROOT::Math::Cylindrical3D digiHitLocalPositionVector(smearedDistanceToWire, smearedZ, simHitLocalPositionVector.phi());
+    debug() << "Local simHit distance to the wire: " << simHitLocalPositionVector.rho() << " Local digiHit distance to the wire: " << smearedDistanceToWire << " in cm" << endmsg;
+    debug() << "Local simHit z: " << simHitLocalPositionVector.z() << " Local digiHit distance to the wire: " << smearedZ << " in cm" << endmsg;
+    debug() << "Local simHit phi: " << simHitLocalPositionVector.phi() << " Local digiHit distance to the wire: " << digiHitLocalPositionVector.Phi() << endmsg;
+    // go back to the global frame
+    double digiHitLocalPosition[3] = {digiHitLocalPositionVector.x(), digiHitLocalPositionVector.y(), digiHitLocalPositionVector.z()};
+    double digiHitGlobalPosition[3] = {0, 0, 0};
+    wireTransformMatrix.LocalToMaster(digiHitLocalPosition, digiHitGlobalPosition);
+    // go back to mm
+    edm4hep::Vector3d digiHitGlobalPositionVector(digiHitGlobalPosition[0] / dd4hep::mm, digiHitGlobalPosition[1] / dd4hep::mm, digiHitGlobalPosition[2] / dd4hep::mm);
+    output_digi_hit.setPosition(digiHitGlobalPositionVector);
   }
+  debug() << "Output Digi Hit collection size: " << output_digi_hits->size() << endmsg;
   return StatusCode::SUCCESS;
 }
 
