aa306b2786a4a031876c18f860c15df2e15209b2
armin-ilg <armin.ilg@cern.ch>
2023-09-26 15:55:10 +0200

    Proper formating

--- VTXdigi/src/VTXdigitizer.cpp
+++ VTXdigi/src/VTXdigitizer.cpp
@@ -6,14 +6,15 @@
 
 DECLARE_COMPONENT(VTXdigitizer)
 
-VTXdigitizer::VTXdigitizer(const std::string& aName, ISvcLocator* aSvcLoc) : GaudiAlgorithm(aName, aSvcLoc), m_geoSvc("GeoSvc", "VTXdigitizer")  {
+VTXdigitizer::VTXdigitizer(const std::string& aName, ISvcLocator* aSvcLoc)
+    : GaudiAlgorithm(aName, aSvcLoc), m_geoSvc("GeoSvc", "VTXdigitizer") {
   declareProperty("inputSimHits", m_input_sim_hits, "Input sim vertex hit collection name");
   declareProperty("outputDigiHits", m_output_digi_hits, "Output digitized vertex hit collection name");
 }
 
 VTXdigitizer::~VTXdigitizer() {}
 
-StatusCode VTXdigitizer::initialize() { 
+StatusCode VTXdigitizer::initialize() {
   // Initialize random services
   if (service("RndmGenSvc", m_randSvc).isFailure()) {
     error() << "Couldn't get RndmGenSvc!" << endmsg;
@@ -40,11 +41,12 @@ StatusCode VTXdigitizer::initialize() {
 
   // set the cellID decoder
   m_decoder = m_geoSvc->lcdd()->readout(m_readoutName).idSpec().decoder();
-  
+
   // retrieve the volume manager
   m_volman = m_geoSvc->lcdd()->volumeManager();
 
-  return StatusCode::SUCCESS; }
+  return StatusCode::SUCCESS;
+}
 
 StatusCode VTXdigitizer::execute() {
   // Get the input collection with Geant4 hits
@@ -59,51 +61,51 @@ StatusCode VTXdigitizer::execute() {
     // smear the hit position: need to go in the local frame of the silicon sensor to smear in the direction along/perpendicular to the stave
 
     // retrieve the cell detElement
-    dd4hep::DDSegmentation::CellID cellID         = input_sim_hit.getCellID();
+    dd4hep::DDSegmentation::CellID cellID = input_sim_hit.getCellID();
     debug() << "Digitisation of " << m_readoutName << ", cellID: " << cellID << endmsg;
 
     //// Leave this for the moment as it could be useful
     // std::string sensorDetElementName = "";
     // if(m_readoutName == "VTXIBCollection"){
     //     sensorDetElementName =
-    //       Form( "VTXIB_layer%d_stave%d_module%d_sensors_sensor%d", 
-    //             m_decoder->get(cellID, "layer"), 
-    //             m_decoder->get(cellID, "stave"), 
-    //             m_decoder->get(cellID, "module"), 
+    //       Form( "VTXIB_layer%d_stave%d_module%d_sensors_sensor%d",
+    //             m_decoder->get(cellID, "layer"),
+    //             m_decoder->get(cellID, "stave"),
+    //             m_decoder->get(cellID, "module"),
     //             m_decoder->get(cellID, "sensor")
     //       );
     // }
     // else if(m_readoutName == "VTXOBCollection"){
     //     sensorDetElementName =
-    //       Form( "VTXOB_layer%d_stave%d_module%d_sensor%d", 
-    //             m_decoder->get(cellID, "layer"), 
-    //             m_decoder->get(cellID, "stave"), 
-    //             m_decoder->get(cellID, "module"), 
+    //       Form( "VTXOB_layer%d_stave%d_module%d_sensor%d",
+    //             m_decoder->get(cellID, "layer"),
+    //             m_decoder->get(cellID, "stave"),
+    //             m_decoder->get(cellID, "module"),
     //             m_decoder->get(cellID, "sensor")
     //       );
     // }
     // else if(m_readoutName == "VTXDCollection"){
     //   sensorDetElementName =
-    //     Form( "VTXD_side%d_layer%d_petal%d_stave%d_sensors_module%d_sensor%d", 
-    //           m_decoder->get(cellID, "side"), 
-    //           m_decoder->get(cellID, "layer"), 
-    //           m_decoder->get(cellID, "petal"), 
-    //           m_decoder->get(cellID, "stave"), 
-    //           m_decoder->get(cellID, "module"), 
+    //     Form( "VTXD_side%d_layer%d_petal%d_stave%d_sensors_module%d_sensor%d",
+    //           m_decoder->get(cellID, "side"),
+    //           m_decoder->get(cellID, "layer"),
+    //           m_decoder->get(cellID, "petal"),
+    //           m_decoder->get(cellID, "stave"),
+    //           m_decoder->get(cellID, "module"),
     //           m_decoder->get(cellID, "sensor")
     //     );
     // }
     // else if(m_readoutName == "VertexBarrelReadout"){ // CLD Barrel
     //   sensorDetElementName =
-    //     Form( "VertexBarrel_layer%d_ladder%d", 
-    //           m_decoder->get(cellID, "layer"), 
+    //     Form( "VertexBarrel_layer%d_ladder%d",
+    //           m_decoder->get(cellID, "layer"),
     //           m_decoder->get(cellID, "module")
     //     );
     // }
 
     // Get transformation matrix of sensor
     const auto& sensorTransformMatrix = m_volman.lookupVolumePlacement(cellID).matrix();
-  
+
     // Retrieve global position in mm and apply unit transformation (translation matrix is stored in cm)
     double simHitGlobalPosition[3] = {input_sim_hit.getPosition().x * dd4hep::mm,
                                       input_sim_hit.getPosition().y * dd4hep::mm,
@@ -127,43 +129,44 @@ StatusCode VTXdigitizer::execute() {
 
     // Smear the hit in the local sensor coordinates
     double digiHitLocalPosition[3];
-    if(m_readoutName=="VTXIBCollection" || m_readoutName=="VTXOBCollection"){ // In barrel, the sensor box is along y-z
+    if (m_readoutName == "VTXIBCollection" ||
+        m_readoutName == "VTXOBCollection") {  // In barrel, the sensor box is along y-z
       digiHitLocalPosition[0] = simHitLocalPositionVector.x();
       digiHitLocalPosition[1] = simHitLocalPositionVector.y() + m_gauss_x.shoot() * dd4hep::mm;
       digiHitLocalPosition[2] = simHitLocalPositionVector.z() + m_gauss_y.shoot() * dd4hep::mm;
-    }
-    else if(m_readoutName == "VTXDCollection"){ // In the disks, the sensor box is already in x-y
+    } else if (m_readoutName == "VTXDCollection") {  // In the disks, the sensor box is already in x-y
       digiHitLocalPosition[0] = simHitLocalPositionVector.x() + m_gauss_x.shoot() * dd4hep::mm;
       digiHitLocalPosition[1] = simHitLocalPositionVector.y() + m_gauss_y.shoot() * dd4hep::mm;
       digiHitLocalPosition[2] = simHitLocalPositionVector.z();
-    }
-    else{
-      error() << "VTX readout name (m_readoutName) needs to be either VTXIBCollection, VTXOBCollection or VTXDCollection" << endmsg;
+    } else {
+      error()
+          << "VTX readout name (m_readoutName) needs to be either VTXIBCollection, VTXOBCollection or VTXDCollection"
+          << endmsg;
       return StatusCode::FAILURE;
     }
 
     // go back to the global frame
     double digiHitGlobalPosition[3] = {0, 0, 0};
     sensorTransformMatrix.LocalToMaster(digiHitLocalPosition, digiHitGlobalPosition);
-    
+
     // go back to mm
     edm4hep::Vector3d digiHitGlobalPositionVector(digiHitGlobalPosition[0] / dd4hep::mm,
                                                   digiHitGlobalPosition[1] / dd4hep::mm,
                                                   digiHitGlobalPosition[2] / dd4hep::mm);
 
-    debug() << "Global digiHit x " << digiHitGlobalPositionVector[0] << " [mm] --> Local digiHit x " << digiHitLocalPosition[0]
-            << " [cm]" << endmsg;
-    debug() << "Global digiHit y " << digiHitGlobalPositionVector[1] << " [mm] --> Local digiHit y " << digiHitLocalPosition[1]
-            << " [cm]" << endmsg;
-    debug() << "Global digiHit z " << digiHitGlobalPositionVector[2] << " [mm] --> Local digiHit z " << digiHitLocalPosition[2]
-            << " [cm]" << endmsg;
+    debug() << "Global digiHit x " << digiHitGlobalPositionVector[0] << " [mm] --> Local digiHit x "
+            << digiHitLocalPosition[0] << " [cm]" << endmsg;
+    debug() << "Global digiHit y " << digiHitGlobalPositionVector[1] << " [mm] --> Local digiHit y "
+            << digiHitLocalPosition[1] << " [cm]" << endmsg;
+    debug() << "Global digiHit z " << digiHitGlobalPositionVector[2] << " [mm] --> Local digiHit z "
+            << digiHitLocalPosition[2] << " [cm]" << endmsg;
     debug() << "Moving to next hit... " << std::endl << endmsg;
 
     output_digi_hit.setEDep(input_sim_hit.getEDep());
     output_digi_hit.setPosition(digiHitGlobalPositionVector);
 
     // Apply time smearing
-    output_digi_hit.setTime( input_sim_hit.getTime() + m_gauss_time.shoot() );
+    output_digi_hit.setTime(input_sim_hit.getTime() + m_gauss_time.shoot());
 
     output_digi_hit.setCellID(cellID);
   }
