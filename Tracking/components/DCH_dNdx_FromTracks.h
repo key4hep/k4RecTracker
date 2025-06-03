a0b9d6ca684da3b8b70512a2e7137dd404bdee07
Andreas Loeschcke Centeno <andreas.loeschcke.centeno@cern.ch>
2025-06-03 09:43:19 +0200

    Added delphes TrkUtil to calculate track length

--- Tracking/components/DCH_dNdx_FromTracks.h
+++ Tracking/components/DCH_dNdx_FromTracks.h
@@ -37,7 +37,34 @@ private:
     SmartIF<IUniqueIDGenSvc> m_uniqueIDSvc{nullptr};
     inline static thread_local std::mt19937_64 m_engine;
 
-    TrkUtil m_delphesTrkUtil;
+    TrkUtil* m_delphesTrkUtil;
+
+    // Drift Chamber (geometry) parameters
+    Gaudi::Property<double> m_Zmin{
+        this,
+        "Zmin",
+        {-2250.0},
+        "smallest Z value inside drift chamber in mm.)"};
+    Gaudi::Property<double> m_Zmax{
+        this,
+        "Zmax",
+        {2250.0},
+        "largest Z value inside drift chamber in mm.)"};
+    Gaudi::Property<double> m_Rmin{
+        this,
+        "Rmin",
+        {349.8},
+        "inner radius of the drift chamber in mm.)"};
+    Gaudi::Property<double> m_Rmax{
+        this,
+        "Rmax",
+        {2015.0},
+        "outer radius of the drift chamber in mm.)"};
+    Gaudi::Property<int> m_GasSel{
+        this,
+        "GasSel",
+        {0},
+        "Gas selection: 0: He(90%)-Isobutane(10%), 1: pure He, 2: Ar(50%)-Ethane(50%), 3: pure Ar."};
 };
 
 #endif // DCH_DNDX_FROM_TRACKS_H
