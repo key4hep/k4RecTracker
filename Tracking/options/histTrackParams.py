898a8e9f3ca8c231ff1ba09b4a3542f3a545f1f7
Victor Schwan <victor.schwan@cern.ch>
2025-07-04 13:43:47 +0200

    Outsource plotting params dict

--- Tracking/options/histTrackParams.py
+++ Tracking/options/histTrackParams.py
@@ -9,30 +9,10 @@ import matplotlib.pyplot as plt
 import uproot
 
 from commonArgParsing import add_common_args, detModNames, registry
+from plotting import linewidth, params, plotGridAlpha
+
 plt.style.use("seaborn-v0_8-colorblind")
 
-# general plotting options
-labelsize = 24
-linewidth = 1.5
-majorTickSize = 10
-plotGridAlpha = .7
-params = {
-    "xtick.direction": "in",
-    "ytick.direction": "in",
-    "xtick.top": True,
-    "ytick.right": True,
-    "xtick.major.size": majorTickSize,  # Tick length
-    "ytick.major.size": majorTickSize,
-    "xtick.major.width": linewidth,  # Tick line width
-    "ytick.major.width": linewidth,
-    "axes.linewidth": linewidth,
-    "legend.fontsize": labelsize,  # "x-large",
-    "axes.labelsize": labelsize,  # "x-large",
-    "axes.titlesize": labelsize,  # "x-large",
-    "xtick.labelsize": labelsize,  # "x-large",
-    "ytick.labelsize": labelsize,  # "x-large",
-    "figure.autolayout": True,
-}  #'figure.figsize': (15, 5),
 mpl.rcParams.update(params)
 
 parser = add_common_args(ArgumentParser())
---
+++ Tracking/options/plotting.py
@@ -0,0 +1,22 @@
+# general plotting options
+labelsize = 24
+linewidth = 1.5
+majorTickSize = 10
+plotGridAlpha = .7
+params = {
+    "xtick.direction": "in",
+    "ytick.direction": "in",
+    "xtick.top": True,
+    "ytick.right": True,
+    "xtick.major.size": majorTickSize,  # Tick length
+    "ytick.major.size": majorTickSize,
+    "xtick.major.width": linewidth,  # Tick line width
+    "ytick.major.width": linewidth,
+    "axes.linewidth": linewidth,
+    "legend.fontsize": labelsize,  # "x-large",
+    "axes.labelsize": labelsize,  # "x-large",
+    "axes.titlesize": labelsize,  # "x-large",
+    "xtick.labelsize": labelsize,  # "x-large",
+    "ytick.labelsize": labelsize,  # "x-large",
+    "figure.autolayout": True,
+}  #'figure.figsize': (15, 5),
