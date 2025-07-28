6c3f0084b7a95036068c74317de84ffaa9b8fbca
Andrea De Vita <adevita@fcc-ironic-02.cern.ch>
2025-07-28 09:41:19 +0200

    ADD onnx model hash

--- Tracking/test/testTrackFinder/test_trackFinder.sh
+++ Tracking/test/testTrackFinder/test_trackFinder.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+MODEL_PATH=$1
+
 XML_FILE=$K4GEO/FCCee/IDEA/compact/IDEA_o1_v03/IDEA_o1_v03.xml
-STEERING_FILE=SteeringFile_IDEA_o1_v03.py
-MODEL_PATH=/eos/project/f/fccsw-web/www/filesForSimDigiReco/IDEA/IDEA_o1_v03/SimpleGatrIDEAv3o1.onnx
+STEERING_FILE=Tracking/test/testTrackFinder/SteeringFile_IDEA_o1_v03.py
 TBETA=0.6
 TD=0.3
 
@@ -11,6 +12,6 @@ ddsim --steeringFile $STEERING_FILE \
       -G --gun.distribution uniform --gun.particle e- \
       --random.seed 42 \
       --numberOfEvents 1 \
-      --outputFile out_sim_edm4hep.root 
+      --outputFile Tracking/test/testTrackFinder/out_sim_edm4hep.root 
     
-k4run runTestTrackFinder.py --inputFile out_sim_edm4hep.root --outputFile out_tracks.root --modelPath $MODEL_PATH --tbeta $TBETA --td $TD
+k4run Tracking/test/testTrackFinder/runTestTrackFinder.py --inputFile Tracking/test/testTrackFinder/out_sim_edm4hep.root --outputFile Tracking/test/testTrackFinder/out_tracks.root --modelPath $MODEL_PATH --tbeta $TBETA --td $TD
