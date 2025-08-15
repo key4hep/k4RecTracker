145372e47acc94cd72f53f77130ddaafcb4f8496
Victor Schwan <victor.schwan@cern.ch>
2025-08-15 17:58:28 +0200

    FORMAT/REFAC: make pylint happy and obey naming convention

--- Tracking/options/ExtractTrackParams.py
+++ Tracking/options/ExtractTrackParams.py
@@ -7,43 +7,43 @@ from Gaudi.Configuration import INFO, VERBOSE
 from k4FWCore import ApplicationMgr, IOSvc
 from k4FWCore.parseArgs import parser
 
-args = add_common_args(parser).parse_known_args()[0]
-assert len(args.detectorModels) == 1, (
-    f"Only provide one detector model! You provided {args.detectorModels}"
+ARGS = add_common_args(parser).parse_known_args()[0]
+assert len(ARGS.detectorModels) == 1, (
+    f"Only provide one detector model! You provided {ARGS.detectorModels}"
 )
-args.detectorModels = args.detectorModels[0]
+ARGS.detectorModels = ARGS.detectorModels[0]
 
-fileSuffix = ".edm4hep.root"
-procName = "TrackParamExtractor"
-basePath = Path(os.getenv("prmDir", Path.home() / "promotion"))
-in_out_base_path = basePath / "data" / procName
-corePath = f"{args.version}_{detModNames[args.detectorModels]}"
+FILE_SUFFIX = ".edm4hep.root"
+PROCESSOR_NAME = "TrackParamExtractor"
+BASE_PATH = Path(os.getenv("prmDir", Path.home() / "promotion"))
+IN_OUT_BASE_PATH = BASE_PATH / "data" / PROCESSOR_NAME
+CORE_PATH = f"{ARGS.version}_{detModNames[ARGS.detectorModels]}"
 
 # assert that the input path exists
-inputPath = (in_out_base_path / "input_data" / f"{corePath}_REC").with_suffix(
-    fileSuffix
+INPUT_PATH = (IN_OUT_BASE_PATH / "input_data" / f"{CORE_PATH}_REC").with_suffix(
+    FILE_SUFFIX
 )
-assert inputPath.exists(), f"ERROR: The input path ({inputPath}) does not exist!"
+assert INPUT_PATH.exists(), f"ERROR: The input path ({INPUT_PATH}) does not exist!"
 
 iosvc = IOSvc()
-iosvc.Input = str(inputPath)
+iosvc.Input = str(INPUT_PATH)
 iosvc.Output = str(
-    (in_out_base_path / "out_track_params" / f"{corePath}_track_params").with_suffix(
-        fileSuffix
+    (IN_OUT_BASE_PATH / "out_track_params" / f"{CORE_PATH}_track_params").with_suffix(
+        FILE_SUFFIX
     )
 )
 # iosvc.outputCommands = ["drop *", "keep SiTrackPhi"]
 
-printer = TrackParamExtractor(procName, nStars=40)
+printer = TrackParamExtractor(PROCESSOR_NAME, nStars=40)
 printer.OutputLevel = VERBOSE
 
 # the collection name with the SiTracks differs between ILC and FCC models
-if registry.get(args.detectorModels).at_fcc:
+if registry.get(ARGS.detectorModels).at_fcc:
     printer.InputSiTracks = ["SiTracksCT"]
-    siTrackCollName = "SiTracksCT"
+    SI_TRACK_COLL_NAME = "SiTracksCT"
 else:
-    siTrackCollName = "SiTracks"
-iosvc.CollectionNames = ["ClupatraTracks", siTrackCollName]
+    SI_TRACK_COLL_NAME = "SiTracks"
+iosvc.CollectionNames = ["ClupatraTracks", SI_TRACK_COLL_NAME]
 
 
 ApplicationMgr(
