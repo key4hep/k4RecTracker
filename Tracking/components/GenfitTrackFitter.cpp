74660ae75409c0318307e9b3422c8a63615d51f9
Andrea De Vita <82283746+andread3vita@users.noreply.github.com>
2026-04-24 16:15:01 +0200

    Update initialize Tracking/components/GenfitTrackFitter.cpp
    Co-authored-by: Brieuc Francois <brieuc.francois@cern.ch>

--- Tracking/components/GenfitTrackFitter.cpp
+++ Tracking/components/GenfitTrackFitter.cpp
@@ -224,7 +224,7 @@ struct GenfitTrackFitter final
 
         } catch (const std::exception& e) {
 
-          warning() << "Failed to retrieve ECal LayeredCalorimeterData: " << e.what()
+          error() << "Failed to retrieve ECal LayeredCalorimeterData: " << e.what()
                     << ". Calorimeter geometry for propagation is not available — please implement it or switch off "
                        "the extrapolation."
                     << endmsg;
