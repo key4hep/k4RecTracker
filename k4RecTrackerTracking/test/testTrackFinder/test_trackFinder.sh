6a23f1811831225a56df9599a438fa551d0d629a
Victor Schwan <victor.schwan@cern.ch>
2026-08-05 12:43:35 +0200

    Fix TrackFinder test

--- k4RecTrackerTracking/test/testTrackFinder/test_trackFinder.sh
+++ k4RecTrackerTracking/test/testTrackFinder/test_trackFinder.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # clean up previous output files
-rm -f Tracking/test/testTrackFinder/out_sim_edm4hep.root
-rm -f Tracking/test/testTrackFinder/out_tracks.root
+rm -f k4RecTrackerTracking/test/testTrackFinder/out_sim_edm4hep.root
+rm -f k4RecTrackerTracking/test/testTrackFinder/out_tracks.root
 
 MODEL_PATH=$1
 
 XML_FILE=$K4GEO/FCCee/IDEA/compact/IDEA_o1_v03/IDEA_o1_v03.xml
-STEERING_FILE=Tracking/test/testTrackFinder/SteeringFile_IDEA_o1_v03.py
+STEERING_FILE=k4RecTrackerTracking/test/testTrackFinder/SteeringFile_IDEA_o1_v03.py
 TBETA=0.6
 TD=0.3
 
@@ -18,7 +18,7 @@ ddsim --steeringFile $STEERING_FILE \
       -G --gun.distribution uniform --gun.particle mu- \
       --random.seed 42 \
       --numberOfEvents 1 \
-      --outputFile Tracking/test/testTrackFinder/out_sim_edm4hep.root
+      --outputFile k4RecTrackerTracking/test/testTrackFinder/out_sim_edm4hep.root
 
-k4run Tracking/test/testTrackFinder/runTestTrackFinder.py --inputFile Tracking/test/testTrackFinder/out_sim_edm4hep.root --outputFile Tracking/test/testTrackFinder/out_tracks.root --modelPath $MODEL_PATH --tbeta $TBETA --td $TD
+k4run k4RecTrackerTracking/test/testTrackFinder/runTestTrackFinder.py --inputFile k4RecTrackerTracking/test/testTrackFinder/out_sim_edm4hep.root --outputFile k4RecTrackerTracking/test/testTrackFinder/out_tracks.root --modelPath $MODEL_PATH --tbeta $TBETA --td $TD
 
