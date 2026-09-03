f2248f42c8e4f8320a100c34fe525b84e3c1fab0
Juan Miguel Carceller <jmcarcell@users.noreply.github.com>
2026-09-03 09:56:10 +0200

    Remove goto statements from TrackdNdx

--- Tracking/components/TrackdNdxDelphesBased.cpp
+++ Tracking/components/TrackdNdxDelphesBased.cpp
@@ -98,86 +98,87 @@ edm4hep::RecDqdxCollection TrackdNdxDelphesBased::operator()(const edm4hep::Trac
       continue;
     }
 
-    { // Scope block to avoid cross initialisation with the goto statements
+    const auto store_value = [&]() {
+      debug() << "dNdx value: " << dNdx_value << " (clusters/mm)" << endmsg;
 
-      //////////////////////////
-      // Particle Information //
-      //////////////////////////
-      double momentum = edm4hep::utils::magnitude(mc_particle.getMomentum());
-      double mass = mc_particle.getMass();
+      auto dqdx = outputCollection.create();
+      edm4hep::Quantity q;
+      q.value = static_cast<float>(dNdx_value);
 
-      double betagamma = momentum / mass;
-      debug() << "MCParticle betagamma: " << betagamma << endmsg;
-      // Check if betagamma is in valid range of delphes parametrisation (status: 16 June 2025)
-      if (betagamma < 0.5 || betagamma > 20000.0) {
-        warning() << "beta*gamma value outside of \"good\" range of delphes parametrisation (0.5-20000), dN/dx will be "
-                     "set to dummy value: "
-                  << dummy_value << " clusters/mm" << endmsg;
-        goto store_value;
-      }
+      dqdx.setDQdx(q);
+      dqdx.setTrack(track);
+    };
 
-      // Get number of clusters per length from delphes
-      // Output from delphes function is in 1/m, so to convert to 1/mm we need to scale accordingly
-      double nclusters_per_mm = m_delphesTrkUtil.Nclusters(betagamma, m_GasSel.value()) / 1000.0;
-      debug() << "Number of clusters per mm: " << nclusters_per_mm << endmsg;
+    //////////////////////////
+    // Particle Information //
+    //////////////////////////
+    double momentum = edm4hep::utils::magnitude(mc_particle.getMomentum());
+    double mass = mc_particle.getMass();
 
-      ///////////////////////
-      // Track Information //
-      ///////////////////////
-      // Use track state at IP, since this corresponds to delphes and energy loss in tracking system is negligible
-      const auto track_state = track.getTrackState(edm4hep::TrackState::AtIP).value();
+    double betagamma = momentum / mass;
+    debug() << "MCParticle betagamma: " << betagamma << endmsg;
+    // Check if betagamma is in valid range of delphes parametrisation (status: 16 June 2025)
+    if (betagamma < 0.5 || betagamma > 20000.0) {
+      warning() << "beta*gamma value outside of \"good\" range of delphes parametrisation (0.5-20000), dN/dx will be "
+                   "set to dummy value: "
+                << dummy_value << " clusters/mm" << endmsg;
+      store_value();
+      continue;
+    }
 
-      // Convert edm4hep::TrackState to delphes parameters
-      // Inverse conversion from
-      // https://github.com/key4hep/k4SimDelphes/blob/main/converter/src/DelphesEDM4HepConverter.cc#L532 Note the same
-      // order of parameters as in delphes: D0, phi, C, Z0, cot(theta) See:
-      // https://github.com/delphes/delphes/blob/98f15add056e657e39bda9e32ccd97ef427ce04c/external/TrackCovariance/TrkUtil.cc#L961
-      TVectorD delphes_track(5);
-      delphes_track[0] = track_state.D0;
-      delphes_track[1] = track_state.phi;
-      const double scale = -2.0; // delphes uses C (half curvature) instead of omega, scale is used to convert
-      delphes_track[2] = track_state.omega / scale;
-      delphes_track[3] = track_state.Z0;
-      delphes_track[4] =
-          track_state.tanLambda; // tanLambda and cot(theta) are the same thing (see DelphesEDM4HepConverter.cc)
+    // Get number of clusters per length from delphes
+    // Output from delphes function is in 1/m, so to convert to 1/mm we need to scale accordingly
+    double nclusters_per_mm = m_delphesTrkUtil.Nclusters(betagamma, m_GasSel.value()) / 1000.0;
+    debug() << "Number of clusters per mm: " << nclusters_per_mm << endmsg;
 
-      // Note: track length will already be in mm, since this is what delphes and the track parametrisation use
-      // so no need to cast it to dd4hep::mm
-      double track_length = m_delphesTrkUtil.TrkLen(delphes_track);
-      // Check if track length calculation was successful
-      if (track_length < std::numeric_limits<double>::epsilon()) {
-        warning() << "Delphes track length calculation returned 0.0, dN/dx will be set to dummy value: " << dummy_value
-                  << " clusters/mm" << endmsg;
-        goto store_value;
-      }
-      debug() << "Track length inside full chamber: " << track_length << " mm" << endmsg;
-      // Apply fill factor to track length
-      track_length *= m_fill_factor.value();
-      debug() << "Track length after applying fill factor: " << track_length << " mm" << endmsg;
+    ///////////////////////
+    // Track Information //
+    ///////////////////////
+    // Use track state at IP, since this corresponds to delphes and energy loss in tracking system is negligible
+    const auto track_state = track.getTrackState(edm4hep::TrackState::AtIP).value();
 
-      /////////////////////////////////////////////
-      // Draw Number of Clusters from Poissonian //
-      /////////////////////////////////////////////
+    // Convert edm4hep::TrackState to delphes parameters
+    // Inverse conversion from
+    // https://github.com/key4hep/k4SimDelphes/blob/main/converter/src/DelphesEDM4HepConverter.cc#L532 Note the same
+    // order of parameters as in delphes: D0, phi, C, Z0, cot(theta) See:
+    // https://github.com/delphes/delphes/blob/98f15add056e657e39bda9e32ccd97ef427ce04c/external/TrackCovariance/TrkUtil.cc#L961
+    TVectorD delphes_track(5);
+    delphes_track[0] = track_state.D0;
+    delphes_track[1] = track_state.phi;
+    const double scale = -2.0; // delphes uses C (half curvature) instead of omega, scale is used to convert
+    delphes_track[2] = track_state.omega / scale;
+    delphes_track[3] = track_state.Z0;
+    delphes_track[4] =
+        track_state.tanLambda; // tanLambda and cot(theta) are the same thing (see DelphesEDM4HepConverter.cc)
 
-      double nclusters_mean = nclusters_per_mm * track_length;
-      std::poisson_distribution<int> poisson_dist(nclusters_mean);
+    // Note: track length will already be in mm, since this is what delphes and the track parametrisation use
+    // so no need to cast it to dd4hep::mm
+    double track_length = m_delphesTrkUtil.TrkLen(delphes_track);
+    // Check if track length calculation was successful
+    if (track_length < std::numeric_limits<double>::epsilon()) {
+      warning() << "Delphes track length calculation returned 0.0, dN/dx will be set to dummy value: " << dummy_value
+                << " clusters/mm" << endmsg;
+      store_value();
+      continue;
+    }
+    debug() << "Track length inside full chamber: " << track_length << " mm" << endmsg;
+    // Apply fill factor to track length
+    track_length *= m_fill_factor.value();
+    debug() << "Track length after applying fill factor: " << track_length << " mm" << endmsg;
 
-      int n_cluster = poisson_dist(random_engine);
-      debug() << "Track has " << n_cluster << " clusters." << endmsg;
+    /////////////////////////////////////////////
+    // Draw Number of Clusters from Poissonian //
+    /////////////////////////////////////////////
 
-      dNdx_value = n_cluster / track_length;
+    double nclusters_mean = nclusters_per_mm * track_length;
+    std::poisson_distribution<int> poisson_dist(nclusters_mean);
 
-    } // end of scope block
+    int n_cluster = poisson_dist(random_engine);
+    debug() << "Track has " << n_cluster << " clusters." << endmsg;
 
-  store_value:
-    debug() << "dNdx value: " << dNdx_value << " (clusters/mm)" << endmsg;
+    dNdx_value = n_cluster / track_length;
 
-    auto dqdx = outputCollection.create();
-    edm4hep::Quantity q;
-    q.value = static_cast<float>(dNdx_value);
-
-    dqdx.setDQdx(q);
-    dqdx.setTrack(track);
+    store_value();
   }
 
   return outputCollection;
